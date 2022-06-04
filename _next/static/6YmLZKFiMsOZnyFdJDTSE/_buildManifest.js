8aeddd0b4a709edb5f3cae69fbcaf7de4de7075d
Byron Wall <byron@byroni.us>
2022-06-04 14:39:21 -0400

    Updates

---
+++ _next/static/6YmLZKFiMsOZnyFdJDTSE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-087dafbe7b5402fc.js","static/chunks/pages/index-1f4999c5c68afd6d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
